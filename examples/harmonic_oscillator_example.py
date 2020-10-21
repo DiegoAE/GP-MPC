faa118fb0ff10c143b7dc3d37e032e16dd7287f1
Diego Agudelo-Espana <dagudelo@tue.mpg.de>
2020-10-21 15:57:36 +0200

    assessing uncertainty propagation subjectively

--- examples/harmonic_oscillator_example.py
+++ examples/harmonic_oscillator_example.py
@@ -13,60 +13,50 @@ path.append(r"./../")
 from gp_mpc import Model, GP
 
 
-def plot():
-    """ Plot comparison of GP prediction with exact simulation
-        on a 2000 step prediction horizon
-    """
-    Nt = 1000
-    x0 = np.array([2., .201])
-
-    cov = np.zeros((2,2))
-    x = np.zeros((Nt,2))
-    x_sim = np.zeros((Nt,2))
-    x_sim2 = np.zeros((Nt,2))
-
-    x[0] = x0
-    x_sim[0] = x0
-    x_sim2[0] = x0
-
-    #gp.set_method('ME')         # Use Mean Equivalence as GP method
-    for i in range(Nt-1):
-        #x_t, cov = gp.predict(x[i], [], cov)
-        #x[i + 1] = np.array(x_t).flatten()
-        x_sim[i+1] = model.integrate(x0=x_sim[i], u=[], p=[])
-        x_sim2[i+1] = x_sim2[i] + model.sampling_time() * np.array([
-                x_sim2[i,1], -1.0 * x_sim2[i,0]])
-
-    plt.figure()
-    ax = plt.subplot(111)
-    ax.plot(x_sim[:,0], x_sim[:,1], 'k-', linewidth=1.0, label='Exact')
-    ax.plot(x_sim2[:,0], x_sim2[:,1], 'b-', linewidth=1.0, label='Euler')
-    ax.set_ylabel('y')
-    ax.set_xlabel('x')
-    plt.legend(loc='best')
-    plt.show()
-
-
-def plot_harmonic_oscillator(time_steps, dt):
+def plot_harmonic_oscillator(time_steps, dt, gp, model):
     plt.figure()
     ax = plt.subplot(111)
 
     # The initial state is Gaussian distributed with the following moments:
     mu = np.array([2., .201])
     Sigma = np.eye(2) * 0.1
+    mean_traj = np.zeros((time_steps, 2))
+
+    # Setting prediction to use moment matching.
+    gp.set_method('EM')
+    approx_mu = np.copy(mu)
+    approx_Sigma = np.copy(Sigma)
+    approx_mean_traj = np.zeros((time_steps, 2))
+    stdY = np.diag(gp._GP__stdY)  # needed to unnormalized predicted covariance.
 
     # Linear dynamics of harmonic oscillator:
     k = 2.0
     A = np.eye(2) + np.array([[0,1],[-k,0]]) * dt
 
-    mean_traj = np.zeros((time_steps, 2))
     for t in range(time_steps):
-        confidence_ellipse(ax, mu, Sigma)
-        mu = A @ mu
-        Sigma = A @ Sigma @ A.T
+        #confidence_ellipse(ax, mu, Sigma)
+        #mu = A @ mu
+        #Sigma = A @ Sigma @ A.T
+        mu = model.integrate(x0=mu, u=[], p=[])
         mean_traj[t, :] = mu
 
-    ax.plot(mean_traj[:,0], mean_traj[:,1], 'k-', linewidth=1.0, label='Mean')
+    confidence_ellipse(ax, np.array(approx_mu),
+            stdY @ np.array(approx_Sigma) @ stdY)
+    for t in range(time_steps):
+
+        # Note that the mean is unnormalized but the cov isn't.
+        pred_mu, pred_Sigma = gp.predict(approx_mu, [], approx_Sigma)
+        approx_mu = pred_mu
+        approx_Sigma = pred_Sigma
+
+        approx_mean_traj[t, :] = np.array(approx_mu).flatten()
+
+    confidence_ellipse(ax, np.array(approx_mu),
+            stdY @ np.array(approx_Sigma) @ stdY)
+
+    ax.plot(mean_traj[:,0], mean_traj[:,1], 'k-', linewidth=1.0, label='GT')
+    ax.plot(approx_mean_traj[:,0], approx_mean_traj[:,1], 'b-', linewidth=1.0,
+            label='MM')
     ax.set_ylabel('x2')
     ax.set_xlabel('x1')
     plt.legend(loc='best')
@@ -93,14 +83,28 @@ def confidence_ellipse(ax, mu, Sigma, n_std=2):
 
 if __name__ == "__main__":
 
-    # Number of states.
-    Nx = 2 
+    """ System Parameters """
+    dt = .01                    # Sampling time
+    Nx = 2                      # Number of states
+    Nu = 0                      # Number of inputs
+    R_n = np.eye(Nx) * 1e-6     # Covariance matrix of added noise
 
-    # Number of inputs.
-    Nu = 0
+    # Limits in the training data
+    ulb = []    # No inputs are used
+    uub = []    # No inputs are used
+    xlb = [-4., -6.]
+    xub = [4., 6.]
 
-    # Covariance matrix of added noise.
-    R_n = np.eye(Nx) * 1e-6
-    dt = 0.01
-    model = Model(Nx=Nx, Nu=Nu, ode=ode, dt=dt, R=R_n, clip_negative=True)
-    plot_harmonic_oscillator(300, dt)
+    N = 40          # Number of training data
+    N_test = 100    # Number of test data
+
+    """ Create simulation model and generate training/test data"""
+    model          = Model(Nx=Nx, Nu=Nu, ode=ode, dt=dt, R=R_n, clip_negative=True)
+    X, Y           = model.generate_training_data(N, uub, ulb, xub, xlb, noise=True)
+    X_test, Y_test = model.generate_training_data(N_test, uub, ulb, xub, xlb, noise=True)
+
+    """ Create GP model and optimize hyper-parameters"""
+    gp = GP(X, Y, mean_func='zero', normalize=True, xlb=xlb, xub=xub, ulb=ulb,
+            uub=uub, optimizer_opts=None)
+    gp.validate(X_test, Y_test)
+    plot_harmonic_oscillator(200, dt, gp, model)
